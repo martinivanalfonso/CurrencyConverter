c755fd90c5d5637be91e64e98547ac5248d33e17
martinivanalfonso <martinivanalfonso@hotmail.com>
2020-04-06 12:10:20 -0300

    Updates

---
+++ precache-manifest.cb2999d3086af243cafb29e180aeb333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e4ce2ae5d183ec925593875a34e663",
+    "url": "/CurrencyConverter/index.html"
+  },
+  {
+    "revision": "2a53f82938dd41668921",
+    "url": "/CurrencyConverter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c75ba8b65cda8e0db5e6",
+    "url": "/CurrencyConverter/static/js/2.7a394609.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CurrencyConverter/static/js/2.7a394609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a53f82938dd41668921",
+    "url": "/CurrencyConverter/static/js/main.c9d1b790.chunk.js"
+  },
+  {
+    "revision": "627f05b9718c884267b8",
+    "url": "/CurrencyConverter/static/js/runtime-main.54cc1d1f.js"
+  }
+]);
